109e614dc80a8ef215dcc4ab506ce0d010599e88
Grant Harris <grantharris@lululemon.com>
2018-08-23 11:53:03 -0700

    dump out event

--- flask_lambda.py
+++ flask_lambda.py
@@ -38,6 +38,7 @@ __version__ = '0.0.4'
 
 
 def make_environ(event):
+    print('this was the event', event)
     environ = {}
     if event['headers']:
         for hdr_name, hdr_value in event['headers'].items():
